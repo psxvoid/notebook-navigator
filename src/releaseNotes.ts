b3d123fd56205a8b324601f2239110e8b4e7d882
Johan Sanneblad <johan@sanneblad.se>
2025-10-09 08:23:04 +0200

    Update releaseNotes.ts

--- src/releaseNotes.ts
+++ src/releaseNotes.ts
@@ -76,13 +76,8 @@ const RELEASE_NOTES: ReleaseNote[] = [
         version: '1.5.4',
         date: '2025-10-09',
         showOnUpdate: false,
-        improved: [
-            'Folder notes now read icon and color from frontmatter if enabled in settings.'
-        ],
-        fixed: [
-            'Improved shortcuts sync between devices.',
-            'Folders with folder notes now display as clickable links in shortcuts area.'
-        ]
+        improved: ['Folder notes now read icon and color from frontmatter if enabled in settings.'],
+        fixed: ['Improved shortcuts sync between devices.', 'Folders with folder notes now display as clickable links in shortcuts area.']
     },
     {
         version: '1.5.3',
