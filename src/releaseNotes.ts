f891edda1091df20238e6d62fbd88d18f88a1ee8
Johan Sanneblad <johan@sanneblad.se>
2025-11-02 19:44:52 +0100

    Update releaseNotes.ts

--- src/releaseNotes.ts
+++ src/releaseNotes.ts
@@ -80,6 +80,7 @@ const RELEASE_NOTES: ReleaseNote[] = [
         new: [
             '==Multi-tag selection!== This is huge! You can now command+click tags to select multiple tags in list pane with "AND", or shift+command+click to select multiple tags with "OR". This updates the search field, and you can save these filters for future use!',
             '==You can now override "Group by" for each tag and folder==. Just click the "Change appearance" toolbar button in list pane.',
+            'New commands: ==Select next file== and ==Select previous file==. Bind them to something like Cmd+left and Cmd+right to quickly navigate between tags and folders.',
             'New setting: ==List Pane > Appearance > Slim item height==. Reduce line spacing in list pane with slim mode enabled.',
             'New setting: ==List Pane > Appearance > Show pinned group header==. Toggle to hide the section header above pinned notes.',
             'New setting: ==Navigation Pane > Appearance > Root folder spacing==. Use it to add extra spacing between root folders and tags in navigation pane. Especially useful when you use background colors with inheritance.',
